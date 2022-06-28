4a26ec8203cce3e96f700f5c45d94507c2b1589b
chris <c.vivancos@codeur.online>
2022-06-28 22:12:24 +0200

    rajout font / debut code css / debut js

---
+++ assets/js/app.js
@@ -0,0 +1,52 @@
+
+window.onscroll = () => {
+	if (window.scrollY > 200) {
+		document.getElementById('nav').style.background = '#0000007e';
+		/*document.getElemenyId('nav').style.backdrop = '#0000007e';*/
+		document.getElementById('nav').style.boxShadow = '1px 4px 13px 0px rgba(0,0,0,0.75)';
+      
+	} else {
+        console.log('inf');
+		document.getElementById('nav').style.background = 'transparent';
+		document.getElementById('nav').style.boxShadow = 'inherit';
+	}
+  };
+/*---------------modale navigation-----------------------------*/
+var burger = document.getElementById('burger');
+var closeNav = document.getElementById('closeNav');
+var mobileNavArea = document.getElementById('mobileNavArea');
+
+burger.addEventListener('click', function(){
+    mobileNavArea.classList.remove('none');
+
+    setTimeout(() => {
+        mobileNavArea.classList.add('openNav');
+    }, 200);
+    
+    this.classList.add('none');
+    closeNav.classList.remove('none');
+})
+
+closeNav.addEventListener('click', function(){
+
+    mobileNavArea.classList.remove('openNav');
+
+    setTimeout(() => {
+        mobileNavArea.classList.add('none');
+    }, 200);
+   
+    this.classList.add('none');
+    burger.classList.remove('none');
+    
+})
+// /* animation du H2 section 1 */
+// var coucou =  document.getElementById('titleH2')
+
+// window.onscroll = function() {scrollH2()};
+
+// function scrollH2(){
+//     if(document.body.scrollTop > 300 || document.documentElement.scrollTop > 600) {
+//         coucou.classList.add("apparitionH2")
+//     }else {
+//         coucou.classList.remove("apparitionH2")
+// }}
