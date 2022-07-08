d23a80a4abb03f950716c853b23e9d64c825490c
chris <c.vivancos@codeur.online>
2022-07-08 22:54:56 +0200

    rajout cursor animé

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,18 @@
+//////////////////////////////////////////////////////////////////////////////////////
+const cursor = document.querySelector('.cursor');
 
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute('style', 'top:'+(e.pageY - 20)+"px; left:"+(e.pageX - 20)+"px;")
+})
+
+document.addEventListener('click', ()=>{
+    cursor.classList.add('expand');
+
+    setTimeout(()=>{
+        cursor.classList.remove("expand");
+    }, 500);
+})
+ 
  window.onscroll = () => {
  	if (window.scrollY > 200) {
 		document.getElementById('nav').style.background = '#0000007e';
