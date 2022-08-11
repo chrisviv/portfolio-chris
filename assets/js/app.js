002e14f70e8ca1fdb056ea3ae02a22c4e82f03d4
chris <c.vivancos@codeur.online>
2022-08-11 22:11:31 +0200

    media 375px refonte

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,16 @@
+///////////////////////////////////SCROLL: LOGO+BURGER///////////////////////////////////////////////////
+window.onscroll = () => {
+    if (window.scrollY > 200) {
+       document.getElementById('nav').style.background = '#0000007e';
+        // document.getElemenyId('nav').style.backdrop = '#0000007e';
+        document.getElementById('nav').style.boxShadow = '1px 4px 13px 0px rgba(0,0,0,0.75)';
+     
+   } else {
+       console.log('inf');
+        document.getElementById('nav').style.background = 'transparent';
+       document.getElementById('nav').style.boxShadow = 'inherit';
+    }
+  };
 //////////////////// Barre de navivagation/////////////////////////////
  var burger = document.getElementById('burger');
  var closeNav = document.getElementById('closeNav');
@@ -39,20 +52,7 @@ document.addEventListener('click', ()=>{
         cursor.classList.remove("expand");
     }, 500);
 });
- ///////////////////////////////////SCROLL: LOGO+BURGER///////////////////////////////////////////////////
- window.onscroll = () => {
- 	if (window.scrollY > 200) {
-		document.getElementById('nav').style.background = '#0000007e';
- 		// document.getElemenyId('nav').style.backdrop = '#0000007e';
- 		document.getElementById('nav').style.boxShadow = '1px 4px 13px 0px rgba(0,0,0,0.75)';
-      
-	} else {
-        // console.log('inf');
- 		document.getElementById('nav').style.background = 'transparent';
-		document.getElementById('nav').style.boxShadow = 'inherit';
- 	}
-   };
-  //////////////////////////////////////////////////////////////////////
+ //////////////////////////////////////////////////////////////////////
   function modalContent(target){
     fetch('assets/js/description.json').then(responce => responce.json().then(responce => {
         for (let i = 0; i < responce.listeProjets.projets.length; i++) {
@@ -107,34 +107,9 @@ document.getElementsByClassName('cross')[0].addEventListener('click' , function(
     document.getElementById('boxModalDesc').classList.add('none');
   });
 document.getElementsByClassName('boxslideshow')[0].addEventListener('click', function() {
-    document.getElementById('boxModalDesc').classList.remove('none')
+    document.getElementById('boxModalDesc').classList.remove('none');
 });
 /*/////////////////////////////////////////////////////////////////////////////////////////////////////////// */
-// gsap.registerPlugin(ScrollTrigger);
-
-// const titleHeader = document.querySelector("h1");
-// const moi = document.querySelector(".moi");
-// const prenom = document.querySelector(".prenom");
-// // const icon = document.querySelector("#mouse-icon");
-// // const box = document.querySelector(".panel-text");
-// // const img = document.querySelector(".panel-image");
-// // const btn = document.querySelector(".btn");
-
-// // mouse icon animation
-// const iconTl = gsap.timeline({ repeat: -1, paused: true });
-// iconTl
-//   .to(
-//     ".scroll",
-//     {
-//       y: 20,
-//       autoAlpha: 0,
-//       transformOrigin: "50% 100%",
-//       duration: 0.7
-//     },
-//     "icon"
-//   )
-//   .to("#boxScroll", { y: 8, duration: 0.7 }, "icon")
-//   .to("#boxScroll", { y: 0, duration: 0.7 }, "icon+=0.7");
 
 
 
@@ -150,94 +125,3 @@ document.getElementsByClassName('boxslideshow')[0].addEventListener('click', fun
 
 
 
-
-
-//   window.addEventListener("scroll", (event) => {
-
-//     //Récupération de la hauteur de l'ecran
-//     let viewport_height = window.innerHeight;
-
-//     //Recupération des informations de position des éléments
-//     let nav = document.querySelector("#nav").getBoundingClientRect();
-//     let header = document.querySelector("#header").getBoundingClientRect();
-//     let propos = document.querySelector("#aPropos").getBoundingClientRect();
-//     let animationLamp = document.querySelector("#comptencesDev").getBoundingClientRect();
-          
-//     console.log(animation);
-
-//     //Déclanchement de l'action quand l'élément arrive à une certaine position dans l'écran
-//     if (animationLamp.y <= viewport_height / 4) {
-//         document.querySelector('.animation').keyframe = " moveLamp";
-//     }
-
-    // console.log(positionDivOne);
-
-// });
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("slideshow");
-//   var dots = document.getElementsByClassName("slide-btn");
-//   if (n > slides.length) {slideIndex = 1}
-//     if (n < 1) {slideIndex = slides.length}
-//     for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// } 
-
-
-//  /* animation du H2 section 1 */
-//  var apparition =  document.getElementById('aPropos')
-
-//  window.onscroll = function() {scrollH2()};
-
-//  function scrollH2(){
-//      if(document.body.scrollTop > 300 || document.documentElement.scrollTop > 600) {
-//          apparition.classList.add("apparitionH2")
-//     }else {
-//          apparition.classList.remove("apparitionH2")
-//  }}
-
-
-// Pour réaliser une animation en Js à revoir
-// var i = 0;
-// function move() {
-//   if (i == 0) {
-//     i = 1;
-//     var elem = document.querySelector(".barLangages");
-//     var width = 1;
-//     var id = setInterval(frame, 10);
-//     function frame() {
-//       if (width >= 100) {
-//         clearInterval(id);
-//         i = 0;
-//       } else {
-//         width++;
-//         elem.style.width = width + "%";
-//       }
-//     }
-//   }
-// }
-//Modale pour la navigation/////////////////////////////////////
-// const modalContainer = document.querySelector(".modal-container"); 
-// const modalTriggers = document.querySelectorAll(".modal-trigger");
-
-// modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal))
-
-// function toggleModal(){
-//   modalContainer.classList.toggle("active")}
