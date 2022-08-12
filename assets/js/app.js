cbf3459ecebab0a8e9d8a3c682e96cb8de1b9fe7
chris <c.vivancos@codeur.online>
2022-08-12 11:40:28 +0200

    resolution pb navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,64 +1,54 @@
-///////////////////////////////////SCROLL: LOGO+BURGER///////////////////////////////////////////////////
-window.onscroll = () => {
-    if (window.scrollY > 200) {
-       document.getElementById('nav').style.background = '#0000007e';
-        // document.getElemenyId('nav').style.backdrop = '#0000007e';
-        document.getElementById('nav').style.boxShadow = '1px 4px 13px 0px rgba(0,0,0,0.75)';
-     
-   } else {
-       console.log('inf');
-        document.getElementById('nav').style.background = 'transparent';
-       document.getElementById('nav').style.boxShadow = 'inherit';
-    }
-  };
 //////////////////// Barre de navivagation/////////////////////////////
- var burger = document.getElementById('burger');
- var closeNav = document.getElementById('closeNav');
- var mobileNavArea = document.getElementById('mobileNavArea');
- let boxslideshow = document.querySelector('.boxslideshow');
-//  let listingProjets = ['Allojyvé', 'TechNews', 'Créatives', 'Commune de Cornod', 'Capgemen', 'Pop Code', 'PortFolio'];
- burger.addEventListener('click', function(){
-     mobileNavArea.classList.remove('none');
- 
-     setTimeout(() => {
-         mobileNavArea.classList.add('openNav');
-     }, 200);
-     
-     this.classList.add('none');
-     closeNav.classList.remove('none');
- })
-closeNav.addEventListener('click', function(){
- 
-     mobileNavArea.classList.remove('openNav');
- 
-     setTimeout(() => {
-         mobileNavArea.classList.add('none');
-     }, 200);
-    
-     this.classList.add('none');
-     burger.classList.remove('none');
-     
- })
- 
- ///////////////////////////////////CURSOR ANIME///////////////////////////////////////////////////
+var burger = document.getElementById('burger');
+var closeNav = document.getElementById('closeNav');
+var mobileNavArea = document.getElementById('mobileNavArea');
+let boxslideshow = document.querySelector('.boxslideshow');
+let navbar = document.querySelectorAll('li');
+
+function closing(){
+    mobileNavArea.classList.remove('openNav');
+
+    setTimeout(() => {
+        mobileNavArea.classList.add('none');
+    }, 200);
+
+
+    burger.classList.remove('none');
+}
+burger.addEventListener('click', function () {
+    mobileNavArea.classList.remove('none');
+
+    setTimeout(() => {
+        mobileNavArea.classList.add('openNav');
+    }, 200);
+
+    this.classList.add('none');
+    closeNav.classList.remove('none');
+})
+closeNav.addEventListener('click', function () {
+    closing();
+    this.classList.add('none');
+
+})
+///////////////////////////////////CURSOR ANIME///////////////////////////////////////////////////
 const cursor = document.querySelector('.cursor');
 document.addEventListener('mousemove', e => {
-    cursor.setAttribute('style', 'top:'+(e.pageY - 20)+"px; left:"+(e.pageX - 20)+"px;")
+    cursor.setAttribute('style', 'top:' + (e.pageY - 20) + "px; left:" + (e.pageX - 20) + "px;")
 });
-document.addEventListener('click', ()=>{
+document.addEventListener('click', () => {
     cursor.classList.add('expand');
 
-    setTimeout(()=>{
+    setTimeout(() => {
         cursor.classList.remove("expand");
     }, 500);
 });
- //////////////////////////////////////////////////////////////////////
-  function modalContent(target){
+//////////////////////////////////////////////////////////////////////
+function modalContent(target) {
     fetch('assets/js/description.json').then(responce => responce.json().then(responce => {
         for (let i = 0; i < responce.listeProjets.projets.length; i++) {
             console.log(responce.listeProjets.projets[i].picture);
             let listejson = responce.listeProjets.projets[i].name;
-            if (target.toLowerCase() === listejson.replace('é','e').toLowerCase()) {
+            if (target.toLowerCase() === listejson.replace('é', 'e').toLowerCase()) {
                 document.querySelector('.titleModal').textContent = responce.listeProjets.projets[i].name;
                 document.querySelector('.projetDesc').textContent = responce.listeProjets.projets[i].description;
                 document.querySelector('.collaboration').textContent = responce.listeProjets.projets[i].collaboration;
@@ -77,14 +67,14 @@ document.addEventListener('click', ()=>{
                         document.querySelector('.tech05').classList.remove('cssTech05');
                     }
                     document.querySelector('.tech04').classList.remove('cssTech04');
-                }else{
-                    if(document.querySelector('.tech04').classList.contains('cssTech04')===false){
+                } else {
+                    if (document.querySelector('.tech04').classList.contains('cssTech04') === false) {
                         document.querySelector('.tech04').classList.add('cssTech04');
                     }
-                    if(responce.listeProjets.projets[i].language5 != ""){
-                    document.querySelector('.tech05').classList.add('cssTech05');
+                    if (responce.listeProjets.projets[i].language5 != "") {
+                        document.querySelector('.tech05').classList.add('cssTech05');
                     }
-                    if(responce.listeProjets.projets[i].language6 != ""){
+                    if (responce.listeProjets.projets[i].language6 != "") {
                         document.querySelector('.tech06').classList.add('cssTech06');
                     }
                 }
@@ -97,31 +87,36 @@ document.addEventListener('click', ()=>{
                 document.querySelector('.moyen4').textContent = responce.listeProjets.projets[i].moyens4;
                 break;
             }
-    }}));
-  }
-boxslideshow.addEventListener('click', function(e){
+        }
+    }));
+}
+boxslideshow.addEventListener('click', function (e) {
     modalContent(e.target.id);
 });
-/*//////////////////////////////////////////////////////// close ModalDesc*/////////////////////////////////*/
-document.getElementsByClassName('cross')[0].addEventListener('click' , function(){
+///////////////////////////////////SCROLL: LOGO+BURGER///////////////////////////////////////////////////
+window.onscroll = () => {
+    if (window.scrollY > 200) {
+        document.getElementById('nav').style.background = '#0000007e';
+        // document.getElemenyId('nav').style.backdrop = '#0000007e';
+        document.getElementById('nav').style.boxShadow = '1px 4px 13px 0px rgba(0,0,0,0.75)';
+
+    } else {
+        console.log('inf');
+        document.getElementById('nav').style.background = 'transparent';
+        document.getElementById('nav').style.boxShadow = 'inherit';
+    }
+};
+/*//////////////////////////////////////////////////////// close ModalDesc*/ ////////////////////////////////*/
+document.getElementsByClassName('cross')[0].addEventListener('click', function () {
     document.getElementById('boxModalDesc').classList.add('none');
-  });
-document.getElementsByClassName('boxslideshow')[0].addEventListener('click', function() {
+});
+document.getElementsByClassName('boxslideshow')[0].addEventListener('click', function () {
     document.getElementById('boxModalDesc').classList.remove('none');
 });
+for (let i = 0; i < navbar.length; i++) {
+    navbar[i].addEventListener('click', function () {
+        closing();
+        closeNav.classList.add('none');
+    })
+}
 /*/////////////////////////////////////////////////////////////////////////////////////////////////////////// */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
